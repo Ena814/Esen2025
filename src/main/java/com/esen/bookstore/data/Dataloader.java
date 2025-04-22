62bfe3028a5b485996cf72c0ca0e2b5c2ee89fa7
Verdes Eniko Andrea <verdes.eniko2004@gmail.com>
2025-04-22 09:37:29 +0200

    Start implementing shell commands

--- src/main/java/com/esen/bookstore/data/Dataloader.java
+++ src/main/java/com/esen/bookstore/data/Dataloader.java
@@ -1,6 +1,7 @@
 package com.esen.bookstore.data;
 
 import com.esen.bookstore.model.Book;
+import com.esen.bookstore.model.BookStore;
 import com.esen.bookstore.repository.BookRepository;
 import com.esen.bookstore.repository.BookstoreRepository;
 import com.fasterxml.jackson.core.type.TypeReference;
@@ -12,23 +13,21 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StreamUtils;
-
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 @Component
 @Slf4j
-
+@RequiredArgsConstructor
 public class Dataloader {
 
     private final BookRepository bookRepository;
     private final BookstoreRepository bookstoreRepository;
 
-    public Dataloader(BookRepository bookRepository, BookstoreRepository bookstoreRepository) {
-        this.bookRepository = bookRepository;
-        this.bookstoreRepository = bookstoreRepository;
-    }
+
 
     @Value("classpath:data/books.json")
     private Resource booksResource;
@@ -44,6 +43,20 @@ public class Dataloader {
             var booksJson = StreamUtils.copyToString(booksResource.getInputStream(), StandardCharsets.UTF_8);
             var books = objectMapper.readValue(booksJson, new TypeReference<List<Book>>(){});
             bookRepository.saveAll(books);
+
+            var bookstoreJson = StreamUtils.copyToString(bookstoresResource.getInputStream(), StandardCharsets.UTF_8);
+            var bookstores = objectMapper.readValue(bookstoreJson, new TypeReference<List<BookStore>>(){});
+
+            bookstores.forEach(bookStore -> bookStore.setInventory(books.stream()
+                            .collect(Collectors.toMap(
+                                    book -> book,
+                                    book -> ThreadLocalRandom.current().nextInt(1, 50)
+                            ))
+                    )
+
+            );
+            bookstoreRepository.saveAll(bookstores);
+
         } catch (IOException e){
             System.out.println("Cannot seed database" + e);
         }
--- src/main/java/com/esen/bookstore/model/BookStore.java
+++ src/main/java/com/esen/bookstore/model/BookStore.java
@@ -1,71 +1,28 @@
 package com.esen.bookstore.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Map;
 
-
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookStore {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(unique = true)
     private String location;
-    private Double placeModifier;
+    private Double priceModifier;
     private Double moneyInCashRegister;
-
     @ElementCollection(fetch = FetchType.EAGER)
+    @JsonIgnore
     private Map<Book, Integer> inventory;
 
-    public BookStore() {
-    }
 
-    public BookStore(Long id, String location, Double placeModifier, Double moneyInCashRegister, Map<Book, Integer> inventory) {
-        this.id = id;
-        this.location = location;
-        this.placeModifier = placeModifier;
-        this.moneyInCashRegister = moneyInCashRegister;
-        this.inventory = inventory;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public Double getPlaceModifier() {
-        return placeModifier;
-    }
-
-    public void setPlaceModifier(Double placeModifier) {
-        this.placeModifier = placeModifier;
-    }
-
-    public Double getMoneyInCashRegister() {
-        return moneyInCashRegister;
-    }
-
-    public void setMoneyInCashRegister(Double moneyInCashRegister) {
-        this.moneyInCashRegister = moneyInCashRegister;
-    }
-
-    public Map<Book, Integer> getInventory() {
-        return inventory;
-    }
-
-    public void setInventory(Map<Book, Integer> inventory) {
-        this.inventory = inventory;
-    }
 }
